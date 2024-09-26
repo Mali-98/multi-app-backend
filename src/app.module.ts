b8a6b2c4574592e1a4978cf4d620ae421c08dd58
mali1998 <mali@arconme.com>
2024-09-26 17:26:45 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://root:password@localhost:27017', {
+      authSource: 'admin',  // Keep this if you are using authentication
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
