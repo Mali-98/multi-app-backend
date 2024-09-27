2b529dbb274503a2d90ef5cb3c993376ca85fbc9
mali1998 <mali@arconme.com>
2024-09-27 19:04:01 +0300

    product entity

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,22 @@
-export class Product {}
+import { Entity, ObjectIdColumn, ObjectId, Column } from 'typeorm';
+
+@Entity({ name: 'product' })
+export class Product {
+    @ObjectIdColumn() // Use ObjectIdColumn for MongoDB
+    id: ObjectId; // MongoDB ObjectId for the product
+
+    @Column({ unique: true })
+    name: string; // Unique name of the product
+
+    @Column()
+    description: string; // Brief description of the product
+
+    @Column()
+    price: number; // Price of the product
+
+    @Column()
+    stock: number; // Quantity of product in stock
+
+    @Column()
+    vendorId: ObjectId; // Reference to the Vendor who sells this product
+}
