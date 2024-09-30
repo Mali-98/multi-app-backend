a0b02bcdffef5c25fd2be05e3474d67080fc4451
mali1998 <mali@arconme.com>
2024-09-30 18:03:29 +0300

    Update cart.module.ts

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -6,9 +6,10 @@ import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { Cart } from './entities/cart.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), TypeOrmModule.forFeature([Cart])],
   controllers: [CartController],
   providers: [CartService, RolesGuard, JwtService, UserService],
 })
